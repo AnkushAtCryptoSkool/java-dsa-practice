40cef943dd79001b5a7c8ecd6146377ee65a9b0d
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2024-09-25 01:25:47 +0530

    dp question

---
+++ src/dp/CountTheNoOfSubSetWithGivenDifference.java
@@ -0,0 +1,45 @@
+package dp;
+
+public class CountTheNoOfSubSetWithGivenDifference {
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,3};
+        int diff = 1;
+        System.out.println(countTheNoOfSubSetWithGivenDiffAns(arr,diff));
+    }
+
+    private static int countTheNoOfSubSetWithGivenDiffAns(int[] arr,int diff) {
+       // eq1  - sum(subset 1) - sum(subset 2) = givenDiff
+        // eq2 - sum(subset 1) + sum(subset 2) = sum of array
+        // sum(subset1) = (givenDiff+sum of array)/2;
+        int arrSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arrSum = arrSum+arr[i];
+        }
+        int sum_subset1 = (diff+arrSum)/2;
+        return countTheNoOfSubSutWithGivenSum(arr,sum_subset1);
+    }
+
+    private static int countTheNoOfSubSutWithGivenSum(int[] arr, int sum) {
+        int T[][] = new int[arr.length+1][sum+1];
+        for (int i = 0; i < arr.length+1; i++) {
+            for (int j = 0; j < sum+1; j++) {
+                if(i==0)
+                    T[i][j] = 0;
+                if(j==0)
+                    T[i][j] = 1;
+            }
+        }
+        for (int i = 1; i < arr.length+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
+                if(arr[i-1] <= j){
+                 T[i][j] = T[i-1][j-arr[i-1]] + T[i-1][j];
+                }
+                else{
+                    T[i][j] = T[i-1][j];
+                }
+            }
+        }
+        return T[arr.length][sum];
+    }
+
+}
---
+++ src/dp/MinimumSubSetSumDiiference.java
@@ -0,0 +1,51 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinimumSubSetSumDiiference {
+    public static void main(String[] args) {
+        int arr[] = {3, 1, 4, 2, 2, 1};
+        System.out.println(minSubSetSumDifferenceFinder(arr));
+    }
+
+    private static int minSubSetSumDifferenceFinder(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        boolean T[][] = new boolean[arr.length+1][sum+1];
+        issubsetWithTargetSumExists(arr,sum,arr.length,T);
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 1; i <= T[0].length/2; i++) {
+            if(T[arr.length][i] == true)
+                temp.add(i);
+        }
+        int minAns = Integer.MAX_VALUE;
+        for (int i = 0; i < temp.size(); i++) {
+            minAns = Math.min(minAns,Math.abs(sum-(2*temp.get(i))));
+        }
+        return minAns;
+    }
+
+    private static void issubsetWithTargetSumExists(int[] arr, int sum, int n,boolean T[][]) {
+        for (int i = 0; i < n+1; i++) {
+            for (int j = 0; j < sum+1; j++) {
+                if(i==0)
+                    T[i][j] = false;
+                if(j==0)
+                    T[i][j] = true;
+            }
+        }
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
+                if(arr[i-1] <= j){
+                   T[i][j] = T[i-1][j-arr[i-1]] || T[i-1][j];
+                }
+                else{
+                    T[i][j] = T[i-1][j];
+                }
+            }
+        }
+    }
+}
